7712f2cc34eaaae5bc1c397b7954bf36f30f6cff
Kenneth Kiiza <kenneth.kiiza@googlemail.com>
2019-02-17 14:58:04 +0000

    Added Controller and View

---
+++ Tutorial.Javascript.Calculator/Tutorial.Javascript.Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial.Javascript.Calculator.Controllers
+{
+    [Route("[Controller]")]
+    public class CalculatorController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tutorial.Javascript.Calculator/Tutorial.Javascript.Calculator/wwwroot/js/jquery.calculator.js
@@ -0,0 +1,42 @@
+﻿$(function () {
+
+    GenerateCalc();
+});
+
+function GenerateCalc() {
+    var $denom = $('#denominator');
+
+    $('.container').on('keyup', $denom, function (e) {
+
+        $('.container').find($('.tbody')).empty();
+
+        if ($($denom).val().length === 0) {
+            $('.timesTable').hide();
+        } else {
+            $('.timesTable').show();
+        }
+
+        var row;
+        var column;
+        var arr = [];
+        var $num = $($denom).val();
+
+        if ($num > 100) {
+            $('.container').find($('.tbody')).text('The number is too big.');
+        } else {
+
+            for (row = 1; row <= $num; row++) {
+                var result = '<tr>';
+                for (column = 1; column <= $num; column++) {
+                    result += '<td width="20px" style="cursor: default" title="' + row + ' x ' + column + '">'
+                        + (row * column) + '</td>';
+                }
+                result += '</tr>';
+                arr.push(result);
+            }
+            $.each(arr, function (i, v) {
+                $('.container').find($('.tbody')).append(v);
+            });
+        }
+    });
+}
